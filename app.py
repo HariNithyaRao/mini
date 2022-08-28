6932c81565c830e8de7ce2930fed0a7f41140df2
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2022-08-28 17:43:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+from keras.models import load_model
+import cv2
+from PIL import Image
+import numpy as np
+
+loaded_model = load_model("C:/Users/harisathwik/Desktop/miniproject/digitsnew.h5")
+
+st.title(" ?????? ")
+sketch = st_canvas(stroke_width=15, height=300, stroke_color="#FFFFFF",background_color="#000000", key="full_app")
+bt = st.button("RESULT")
+if bt:
+    if sketch.image_data is not None:
+        if sketch.image_data is not None:
+            img = cv2.resize(sketch.image_data.astype(np.uint8), (28, 28))
+            img_rescalling = cv2.resize(img, dsize=(200, 200), interpolation=cv2.INTER_NEAREST)
+            st.image(img_rescalling)
+            x_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            y_pred = loaded_model.predict(x_img.reshape(1, 28, 28))
+            st.write(f"result: {np.argmax(y_pred[0])}")
+            st.bar_chart(y_pred[0])
---
+++ savemodel.py
@@ -0,0 +1,27 @@
+import pickle
+import tensorflow as tf
+from tensorflow.keras.layers import Flatten, Dense
+import numpy as np
+
+dtst = tf.keras.datasets.mnist
+(train_feature, train_target), (test_feature, test_target) = dtst.load_data()
+
+train_feature = train_feature/255.0
+test_feature = test_feature/255.0
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28,1)))
+model.add(tf.keras.layers.Dense(300, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.Dense(50, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.3))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=tf.keras.optimizers.Adam(0.0003),
+              metrics=['accuracy'])
+
+model.fit(train_feature, train_target, batch_size=32, epochs=20)
+# filename = open("C:/Users/harisathwik/Desktop/miniproject/digit_model.pickle", "wb")
+# pickle.dump(model, filename)
+# filename.close()
+model.save("C:/Users/harisathwik/Desktop/miniproject/digitsnew.h5")
